223256f5401608a844d24decf394e7cc640116eb
Darkanmoore <schettini800@gmail.com>
2013-11-19 18:35:55 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -19,10 +19,8 @@ class Example(QtGui.QMainWindow):
     infoAction.setShortcut('Ctrl+I')
     infoAction.setStatusTip('Info application')
     infoAction.triggered.connect(self.infoout)
+
     
-    label = QtGui.QLabel("Downloader Menu Mensa", self)
-    label.move(50,30)
-    label.resize(label.sizeHint())
     
     menu = QtGui.QMenuBar(self)
     exit = menu.addMenu('&File')
@@ -31,7 +29,7 @@ class Example(QtGui.QMainWindow):
     info.addAction(infoAction)
     
     button= QtGui.QPushButton("Download Menu", self)
-    button.move(50, 80)
+    button.move(70, 80)
     button.clicked.connect(self.onActive)
     
     
@@ -47,37 +45,18 @@ class Example(QtGui.QMainWindow):
     
     
   def mese(self, i):
-    if i == 1:
-      i = "gennaio"
-    elif i == 2:
-      i = "febraio"
-    elif i == 3:
-      i = "marzo"
-    elif i == 4:
-      i = "aprile"
-    elif i == 5:
-      i = "maggio"
-    elif i == 6:
-      i = "giugno"
-    elif i == 7:
-      i = "luglio"
-    elif i == 8:
-      i = "agosto"
-    elif i == 9:
-      i = "settembre"
-    elif i == 10:
-      i = "ottobre"
-    elif i == 11:
-      i = "novembre"
-    elif i == 12:
-      i = "dicembre"
-    return i 
+    mesi = {1:"gennaio", 2:"febbraio", 3:"marzo", 4:"aprile",5:"maggio", 6:"giugno", 7:"luglio", 
+	    8:"agosto", 9:"settembre", 10:"ottobre", 11:"novembre", 12:"dicembre"}
+    return mesi.get(i)
    
   def onActive(self):
     d = date.today().day
     m = date.today().month
     M = self.mese(m)
-    webbrowser.open("http://www.adisu.sa.it/fileadmin/user_upload/menu/menu_del_giorno_%d_"% d + M +"__2013_img.pdf")
+    try:
+      webbrowser.open("http://www.adisu.sa.it/fileadmin/user_upload/menu/menu_del_giorno_%d_"% d + M +"__2013_img.pdf")
+    except webbrowser.Error:
+      print(webbrowser.Error)
     
 def main():
   app = QtGui.QApplication(sys.argv)
